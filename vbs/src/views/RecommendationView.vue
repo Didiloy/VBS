ce75770ed3aaab82f56c03be0f9f06dfdcfeca9e
Didiloy <didiloy2001@gmail.com>
2022-05-15 16:28:53 +0200

    recommandations par auteur

--- vbs/src/views/RecommendationView.vue
+++ vbs/src/views/RecommendationView.vue
@@ -1,8 +1,11 @@
 <template>
   <div v-if="Object.keys(books).length > 0">
     <!-- Design here -->
-    <h1 class="text-h4">Recommendations par auteur</h1>
-    <p class="text-subtitle-1">{{ this.choosenAuthor }}</p>
+    <!-- Recommandation par auteur -->
+    <h1 class="text-h4 ml-5">Plus du même auteur</h1>
+    <v-chip-group active-class="accent white--text" column>
+      <v-chip class="ml-5">{{ this.choosenAuthor }}</v-chip>
+    </v-chip-group>
     <div class="d-flex wrapper" style="overflow-x: auto" height="400">
       <v-card
         v-for="book in this.authorsBooks"
@@ -39,8 +42,47 @@
         </v-card-actions>
       </v-card>
     </div>
-    <h1 class="text-h4 mt-5">Recommendations par genre</h1>
-    <p class="text-subtitle-1">{{ this.choosenAuthor }}</p>
+    <!-- Recommandations par genre -->
+    <h1 class="text-h4 mt-5 ml-5">Plus du même genre</h1>
+    <v-chip-group active-class="accent white--text" column>
+      <v-chip class="ml-5">{{ this.choosenCategory }}</v-chip>
+    </v-chip-group>
+    <div class="d-flex wrapper" style="overflow-x: auto" height="400">
+      <v-card
+        v-for="book in this.genreBooks"
+        :key="book.id"
+        class="mx-2 secondary rounded-xl"
+        elevation="6"
+        max-width="380"
+        max-height="320"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          width="380"
+          max-width="380px"
+          :src="
+            book.volumeInfo.imageLinks
+              ? book.volumeInfo.imageLinks.thumbnail
+              : 'https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-1-scaled-1150x647.png'
+          "
+        />
+        <v-img />
+        <v-card-title>
+          {{
+            book.volumeInfo.title.length > 35
+              ? book.volumeInfo.title.slice(0, 35) + ".."
+              : book.volumeInfo.title
+          }}
+        </v-card-title>
+        <v-card-actions class="justify-end">
+          <v-btn color="accent" text @click="Information(book.id)">
+            <v-icon class="accent--text">add</v-icon>
+            informations
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </div>
   </div>
   <div v-else>
     <v-container class="d-flex flex-column justify-center align-center">
@@ -64,6 +106,8 @@ export default {
       books: null,
       authorsBooks: null,
       choosenAuthor: null,
+      genreBooks: null,
+      choosenCategory: null,
     };
   },
   components: {},
@@ -73,8 +117,13 @@ export default {
       let keys = Object.keys(this.books);
       let author =
         this.books[keys[(keys.length * Math.random()) << 0]].volumeInfo.author;
+      let genre =
+        this.books[keys[(keys.length * Math.random()) << 0]].volumeInfo
+          .categories[0];
       this.choosenAuthor = author;
+      this.choosenCategory = genre;
       await this.getBooksByauthor(author);
+      await this.getBooksByGenre(genre);
     });
   },
   methods: {
@@ -94,6 +143,15 @@ export default {
     },
     Information(id) {
       router.replace("/information/" + id);
+    },
+    async getBooksByGenre(genre) {
+      await globalSearch("subject:" + genre)
+        .then((response) => {
+          this.genreBooks = response.items;
+          console.log("genreBooks");
+          console.log(this.genreBooks);
+        })
+        .catch((err) => console.log(err));
     },
   },
   computed: {
